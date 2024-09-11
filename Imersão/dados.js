42a7eccc7f046a48c18bf817fbb79cb7b0bde0bb
Grazi86 <grazielalves@hotmail.com>
2024-09-11 20:57:34 -0300

    1ºversão

---
+++ Imersão/dados.js
@@ -0,0 +1,11 @@
+let dados = [
+    {
+        titulo: "Graziela Pinheiro Alves",
+        descricao: "Graziela, professora, mergulhadora, mãe de duas ratas (Carocas e Soso), dois gatos (Esqueleto e Frida) e madrastinha da Amora. Conhecida como Grazi, ela tem se dedicado atualmente a estudar T.I. e a sua futura esposa Maria Carolina. Entre seus planos estão a construção de uma residência em Beberibe/CE e adquirir um motorhome com sua amada.",
+        tags: "noiva Gra vida paixão Grah Grazi namorada amor Rodrigues"
+    }
+
+]
+
+
+
